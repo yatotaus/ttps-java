ecffc7389fa3b5163db63a2a5cb5412c52983766
Johana Taus <yato_t@live.com.ar>
2016-11-26 15:13:35 -0300

    usuario

--- CarteleraVirtual/src/main/java/Daos/ComentarioDAO.java
+++ CarteleraVirtual/src/main/java/Daos/ComentarioDAO.java
@@ -2,6 +2,55 @@ package Daos;
 
 import java.io.Serializable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import clases.Comentario;
+
 public class ComentarioDAO<T> extends GenericoDAOHibernateJPA<T> {
 	
+	public void agregar(Comentario cart){
+		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("juan");
+
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction etx = em.getTransaction();
+	
+		etx.begin();
+		
+		Comentario c = new Comentario();
+		c.setTexto("Otra cartelera");
+		em.persist(c);
+		etx.commit();
+		em.close();
 	}
+
+	public void actualizar(Class<T> Objeto) {
+		
+	}
+
+	public void borrar(Class<T> Objeto) {
+		
+	}
+
+	public void borrar(Serializable Objeto) {
+		
+	}
+
+	public void persistir(Class<T> Objeto) {
+		
+	}
+
+	public void recuperar(Class<T> Objeto) {
+		
+	}
+
+	public void existe(Class<T> Objeto) {
+		
+	}
+	
+}
--- CarteleraVirtual/src/main/java/Daos/UsuarioDAO.java
+++ CarteleraVirtual/src/main/java/Daos/UsuarioDAO.java
@@ -1,5 +1,55 @@
 package Daos;
 
-public class UsuarioDAO<T> extends GenericoDAOHibernateJPA<T> {
+import java.io.Serializable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import clases.Usuario;
+
+public class UsuarioDAO<T> extends GenericoDAOHibernateJPA<T> {
+	
+	public void agregar(Usuario cart){
+		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("juan");
+
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction etx = em.getTransaction();
+	
+		etx.begin();
+		
+		Usuario c = new Usuario();
+		c.setUsuario("Pepe");
+		em.persist(c);
+		etx.commit();
+		em.close();
+	}
+
+	public void actualizar(Class<T> Objeto) {
+		
+	}
+
+	public void borrar(Class<T> Objeto) {
+		
+	}
+
+	public void borrar(Serializable Objeto) {
+		
+	}
+
+	public void persistir(Class<T> Objeto) {
+		
+	}
+
+	public void recuperar(Class<T> Objeto) {
+		
+	}
+
+	public void existe(Class<T> Objeto) {
+		
+	}
 }
