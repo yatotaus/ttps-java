cf630783e05daf00e91a4fa1dc2b815c3b2d88ae
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2016-11-26 16:36:23 -0300

    Refactoring

--- CarteleraVirtual/src/main/java/Daos/CarteleraDAO.java
+++ CarteleraVirtual/src/main/java/Daos/CarteleraDAO.java
@@ -7,36 +7,7 @@ import clases.Cartelera;
 
 public class CarteleraDAO<T> extends GenericoDAOHibernateJPA<T> {
 	
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("juan");
-	EntityManager em = emf.createEntityManager();
-	
-	
-	public void agregar(Cartelera cart){
-		
-		
-		EntityTransaction etx = em.getTransaction();
-	
-		etx.begin();
-		
-		Cartelera c = new Cartelera();
-		c.setNombreCartelera("AyED");
-		em.persist(c);
-		etx.commit();
-		em.close();
-	}
 
-	public void actualizar(Class<T> Objeto) {
-		EntityTransaction etx = em.getTransaction();
-		
-		etx.begin();
-		
-		Cartelera c = new Cartelera();
-		c.setNombreCartelera("AyED");
-		c.setId(2);
-		em.merge(c);
-		etx.commit();
-		em.close();
-	}
 
 	public void borrar(Class<T> Objeto) {
 		
--- CarteleraVirtual/src/main/java/Daos/GenericoDAOHibernateJPA.java
+++ CarteleraVirtual/src/main/java/Daos/GenericoDAOHibernateJPA.java
@@ -2,12 +2,34 @@ package Daos;
 
 import java.io.Serializable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import Interface.GenericDAO;
+import clases.Cartelera;
 
 public class GenericoDAOHibernateJPA<T> implements GenericDAO<T>{
 
-	public void actualizar(Class<T> Objeto) {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("juan");
+	EntityManager em = emf.createEntityManager();
+	
+	
+	public void agregar(Object cartelera){
 		
+		EntityTransaction etx = em.getTransaction();
+		etx.begin();
+		em.persist(cartelera);
+		etx.commit();
+		em.close();
+	}
+	
+	public void actualizar(Class<T> objetoPersistible) {
+		EntityTransaction etx = em.getTransaction();
+		etx.begin();
+		em.merge(objetoPersistible);
+		etx.commit();
+		em.close();
 	}
 
 	public void borrar(Class<T> Objeto) {
@@ -29,6 +51,8 @@ public class GenericoDAOHibernateJPA<T> implements GenericDAO<T>{
 	public void existe(Class<T> Objeto) {
 		
 	}
+	
+
 
 
 }
