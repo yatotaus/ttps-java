e17a85288a54c40b4ac63c7115165bce9decefca
Marcos Matias <marcosmatias_iriarte@hotmail.com>
2016-12-22 12:13:57 -0300

    Revert "Borrado por entidad y por id de la entidad. Utilización del"
    This reverts commit 758846aa4a9c46886fa8ea2840a74d8c7b6bfaa0.

--- CarteleraVirtual/src/main/java/Interface/GenericDAO.java
+++ CarteleraVirtual/src/main/java/Interface/GenericDAO.java
@@ -4,7 +4,6 @@ public interface GenericDAO<T> {
 	public void agregar(T cartelera);
 	public T actualizar(T Objeto);
 	public void eliminar(T entity);
-	public T eliminar(int id);
 	public void persistir(T Objeto);
 	public void recuperar(T Objeto);
 	public void existe(T Objeto);
--- CarteleraVirtual/src/main/java/TestPersistencia/TestComentario.java
+++ CarteleraVirtual/src/main/java/TestPersistencia/TestComentario.java
@@ -7,44 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Daos.*;
-import clases.*;
-
-
 
 @WebServlet("/TestComentario")
 public class TestComentario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private FactoryDAO fd = new FactoryDAO();
 
     public TestComentario() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		UsuarioDAO userDAO =fd.getUsuarioDAO();
-		
-		Usuario eliminarId = new UsuarioOnline();
-		Usuario eliminarEntidad = new Usuario();
-		Usuario dejar = new Admin();
-		
-		
-		eliminarId.setEmail("Usuario se se va a eliminar por id");
-		eliminarEntidad.setEmail("Usuario se se va a eliminar por entidad");
-		dejar.setEmail("Usuario que no se borra");
-		
-		userDAO.agregar(eliminarEntidad);
-		userDAO.agregar(dejar);
-		userDAO.agregar(eliminarId);
-		
-		userDAO.eliminar(eliminarEntidad);
-		userDAO.eliminar(eliminarId.getId());
-		
-		
-		
-		
-		response.getWriter().append("Terminé el test");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
